fe26938808c1fcdae4ff26167cb720e7e20eed47
Marc Yang <m.y.yang001@gmail.com>
2018-07-21 23:50:58 -0400

    write the dp tabulation solution for edit distance

--- algorithms/EditDistance.java
+++ algorithms/EditDistance.java
@@ -23,14 +23,16 @@ public class EditDistance {
 
 	public static void main(String[] args) {
 
-		// String str1 = "sunday";
-		// String str2 =  "saturday";
-		String str1 = "abaaba";
-		String str2 =  "bababaa";
+		String str1 = "sunday";
+		String str2 =  "saturday";
+		// String str1 = "abaaba";
+		// String str2 =  "bababaa";
 
 
 		System.out.println(editDistance(str1,str2, str1.length(), str2.length()));
+		//editDistanceDP(String str1, String str2, int m, int n)
 
+		System.out.println(editDistanceDP(str1,str2, str1.length(), str2.length()));
 	}
 
 
@@ -62,7 +64,42 @@ public class EditDistance {
 
 	// program the dp in tabulation 
 	// store the results of the subproblems 
-	
+
+	public static int editDistanceDP(String str1, String str2, int m, int n) {
+
+		int[][] lookup = new int[m+1][n+1];
+
+		for (int i = 0; i <= m; i++) {
+
+			for (int j = 0; j <= n; j++) {
+
+
+				// if the first character is empty, then the option is
+				// to insert in the second character of the string 
+				if (i == 0) {
+					lookup[i][j] = j;
+				}
+				else if (j == 0) {
+					lookup[i][j] = i;
+				}
+				// the character of str1 and str2 matches 
+				else if (str1.charAt(i-1) == str2.charAt(j-1)) {
+					lookup[i][j] = lookup[i-1][j-1];
+				}
+
+				else {
+
+					// insert lookup[i][j-1] 
+					// remove lookup[i-1][j] 
+					// replace lookup[i-1][j-1]
+					lookup[i][j] = 1 + Math.min(Math.min(lookup[i][j-1], lookup[i-1][j]), Math.min(lookup[i-1][j], lookup[i-1][j-1]));
+				}
+			}
+
+		}
+
+		return lookup[m][n];
+	}
 
 
 }
